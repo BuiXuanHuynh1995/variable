2e02ee411ec0e2a1eebb64092b692a33b2228c6a
BuiXuanHuynh1995 <huynhxuanbui@gmail.com>
2021-03-04 10:38:23 +0700

    primitive data type

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean valid;   //default = false;
+        boolean active = true;
+        char c = 'h';
+        short s = 1;    //[-2^15 - 2^15 -1]
+        int i = 10;     //[-2^31 - 2^31 -1]
+        long l = 10;    //[-2^63 - 2^63 -1]
+        double d = 10d;
+        float f = 10f;
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(f);
+    }
+
+}
